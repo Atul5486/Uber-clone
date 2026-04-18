63371a978f739b759c0a3d422599d7010c2093d1
Atul5486 <atulkoushal5486@gmail.com>
2026-04-18 15:51:27 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
+import Start from './pages/Start'
+import CaptainHome from './pages/CaptainHome'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import UserLogout from './pages/UserLogout'
+import Riding from './pages/Riding'
+import CaptainRiding from './pages/CaptainRiding'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Routes>
+        <Route path='/' element={<Start/>}/>
+        <Route path='/signup' element={<UserSignup/>}/>
+        <Route path='/login' element={<UserLogin/>}/>
+        <Route path='/riding' element={<Riding />} />
+        <Route path='/captain-riding' element={<CaptainRiding />} />
+        <Route path='/captain-signup' element={<CaptainSignup/>}/>
+        <Route path='/captain-login' element={<CaptainLogin/>}/>
+         <Route path='/home'
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          } />
+         <Route path='/user/logout'
+          element={
+            <UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+          } />
+           <Route path='/captain-home' element={
+          <CaptainProtectWrapper>
+            <CaptainHome />
+          </CaptainProtectWrapper>
+
+        } />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
